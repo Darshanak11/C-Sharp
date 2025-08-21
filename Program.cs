e544d96e944b0e3ba0990f00cc4dd014ee679bdc
Darshana K <darshanak884@gmail.com>
2025-08-21 09:25:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using ClassLibrary1;        
+
+namespace N
+{ class Program
+    {
+        public void Main()
+        {
+            point p = new point();
+            p.Display();
+        }
+    }
+}
